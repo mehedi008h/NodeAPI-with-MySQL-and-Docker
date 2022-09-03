390f64a962da7f626138c9f91a9861cf111b9da3
Mehedi Hasan <mehedi08h@gmail.com>
2022-09-03 09:25:48 +0600

    setup routes

--- src/routes/patient.routes.ts
+++ src/routes/patient.routes.ts
@@ -1,5 +1,20 @@
 import { Router } from "express";
+import {
+    createPatient,
+    deletePatient,
+    getPatient,
+    getPatients,
+    updatePatient,
+} from "../controller/patient.controller";
 
 const patientRoutes = Router();
+
+patientRoutes.route("/").get(getPatients).post(createPatient);
+
+patientRoutes
+    .route("/:patientId")
+    .get(getPatient)
+    .put(updatePatient)
+    .delete(deletePatient);
 
 export default patientRoutes;
